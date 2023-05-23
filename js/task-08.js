22327e7b8c3f99aee5139addf7af3236bdd3ab1a
Eugeshakw <eugeshakw@gmail.com>
2023-05-24 00:40:57 +0300

    up

--- js/task-08.js
+++ js/task-08.js
@@ -1,19 +1,19 @@
 const form = document.querySelector('.login-form')
 
 
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', event =>{
     event.preventDefault();
     const emailEl = form.elements.email;
     const passwordEl = form.elements.password;
-
-
-    if (emailEl.value.trim() === "" || passwordEl.value.trim() ===" ") {
-        alert("all fields must be filled in");
-        return;
-    } 
-    const obj = {};
-    obj[emailEl.name] = emailEl.value.trim();
-    obj[passwordEl.name] = passwordEl.value.trim();
+    if(emailEl.value.trim() === '' || passwordEl.value.trim() === '') {
+        alert('Please enter')
+        return
+    }
+        const obj = {}
+    obj.email = emailEl.value;
+    obj.password = passwordEl.value;
     console.log(obj);
     form.reset();
-})
+    
+
+});
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const btnEl = document.querySelector('.change-color');
+const spEl = document.querySelector('.color');
+
+btnEl.addEventListener('click', item => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  spEl.textContent = document.body.style.backgroundColor;
+});
